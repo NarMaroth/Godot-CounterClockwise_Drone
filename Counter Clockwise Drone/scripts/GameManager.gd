7bf8626d00532b0d02f02520c9b1034e087f3891
NarMaroth <75537669+NarMaroth@users.noreply.github.com>
2021-11-04 16:56:52 -0300

    Added Missile rotation towards next pos + more
    - know the path its updated by a timer that is a child of the missile.
    - The missile rotate towards the next path point when it changes
    - Try experimenting with using an kinematicbody2d for the Missile movement

--- Counter Clockwise Drone/scripts/GameManager.gd
+++ Counter Clockwise Drone/scripts/GameManager.gd
@@ -2,18 +2,23 @@ extends Node2D
 
 export(NodePath) var drone # a "path" to the drone node in the scene
 
+var con_res
+
 signal start_missile(target)
 
 
+# find Missile Node and connect the "start_missile" signal to "_on_Node2D_start_missile" func 
+#in the Missile script
 func _ready():
 	for node in get_children():
 		var node_script = node.get_script()
 		if node_script != null:
-			if node_script.get_path().get_file() == "Missile.gd":
-				connect("start_missile",node,"_on_Node2D_start_missile") 
+			if node_script.get_path().get_file() == "Missile.gd": #KinematicMissile.gd
+				con_res = connect("start_missile",node,"_on_Node2D_start_missile")
+				if(con_res != OK):
+					print("failed to connect to node")
 				
-	# find Missile Node and connect the "start_missile" signal to "_on_Node2D_start_missile" func 
-	#in the Missile script
+				
 	
 	emit_signal("start_missile",get_node(drone))
 
--- Counter Clockwise Drone/scripts/KinematicMissile.gd
+++ Counter Clockwise Drone/scripts/KinematicMissile.gd
@@ -1,35 +1,37 @@
-extends Sprite
+extends KinematicBody2D
 
-export(float) var move_speed = 100.0;
-export(float) var rotation_speed = 5.0
+# move always forwards and rotate towards the path[0] position 
+
+export(float) var move_speed = 1000.0;
+export(float) var rotation_speed = 100.0
 export(float) var trigger_distance = 10.0;
 
-var reached_current_target : bool = false
 var current_target
 var target_angle
 var path;
+var reached_path_end : bool = false
 
 onready var nav2D = get_parent().find_node("Navigation2D")
 
 func _ready():
+	rotation_speed = deg2rad(rotation_speed)
 	set_process(false)
 
 func _process(delta):
+	
 	update_navigation_path(global_position, current_target.global_position)
 		
+	target_angle = path[0].angle_to_point(position)
 	if (rotation != target_angle):
 		rotate_towards_next_path_pos(delta)
-	
-	move_along_path(move_speed * delta)
+		
+	move_forward(delta)
 	
 	if(reached_target()):
-		$"Update Navigation Timer".stop()
 		set_process(false)
 
+	
 
-func move_along_path(distance):
-	var distance_between_points = position.distance_to(path[0])
-	position = position.linear_interpolate(path[0], distance / distance_between_points)
 
 func rotate_towards_next_path_pos(delta):
 	var angle_dif = -short_angle_dist(rotation, target_angle)
@@ -42,6 +44,9 @@ func short_angle_dist(from, to):
 	var max_angle = TAU
 	var difference = fmod(to - from, max_angle)
 	return fmod(2 * difference, max_angle) - difference
+	
+func move_forward(delta):
+	move_and_slide(Vector2(1, 0).rotated(rotation) * move_speed * delta)
 
 func reached_target()->bool:
 	var distance_between_points = position.distance_to(path[0])
@@ -50,21 +55,11 @@ func reached_target()->bool:
 			return true
 	return false
 
-
 func update_navigation_path(var start_position, var end_position):
 	path = nav2D.get_simple_path(start_position,end_position, true)
 	path.remove(0)
-	
 
 
 func _on_Node2D_start_missile(target):
 	current_target = target
-	update_navigation_path(global_position, target.global_position)
-	target_angle = path[0].angle_to_point(position)
-	$"Update Navigation Timer".start()
 	set_process(true)
-
-
-func _on_Update_Navigation_Timer_timeout():
-	update_navigation_path(global_position, current_target.global_position)
-	target_angle = path[0].angle_to_point(position)
